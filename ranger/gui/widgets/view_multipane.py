a984073b85524eaf2c60e38337c90dbec69153be
phantasea <phantasea@gmail.com>
2020-04-24 23:31:14 +0300

    view_multipane.py: support vline border for multipane mode

--- ranger/gui/widgets/view_multipane.py
+++ ranger/gui/widgets/view_multipane.py
@@ -3,11 +3,13 @@
 
 from __future__ import (absolute_import, division, print_function)
 
+import curses
 from ranger.gui.widgets.view_base import ViewBase
 from ranger.gui.widgets.browsercolumn import BrowserColumn
+from ..displayable import DisplayableContainer
 
 
-class ViewMultipane(ViewBase):  # pylint: disable=too-many-ancestors
+class ViewMultipane(ViewBase):
 
     def __init__(self, win):
         ViewBase.__init__(self, win)
@@ -15,6 +17,8 @@ class ViewMultipane(ViewBase):  # pylint: disable=too-many-ancestors
         self.fm.signal_bind('tab.layoutchange', self._layoutchange_handler)
         self.fm.signal_bind('tab.change', self._tabchange_handler)
         self.rebuild()
+        
+        self.old_draw_borders = self.settings.draw_borders
 
     def _layoutchange_handler(self):
         if self.fm.ui.browser == self:
@@ -26,13 +30,12 @@ class ViewMultipane(ViewBase):  # pylint: disable=too-many-ancestors
                 signal.old.need_redraw = True
             if signal.new:
                 signal.new.need_redraw = True
+            self.rebuild()
 
     def rebuild(self):
         self.columns = []
+        self.container = []
 
-        for child in self.container:
-            self.remove_child(child)
-            child.destroy()
         for name, tab in self.fm.tabs.items():
             column = BrowserColumn(self.win, 0, tab=tab)
             column.main_column = True
@@ -53,3 +56,46 @@ class ViewMultipane(ViewBase):  # pylint: disable=too-many-ancestors
             left += column_width + 1
             column.need_redraw = True
         self.need_redraw = True
+
+    def _draw_borders(self, border_types):
+        win = self.win
+
+        self.color('in_browser', 'border')
+        for child in self.columns[:-1]:
+            if 'separators' in border_types or 'outline' in border_types:
+                try:
+                    win.vline(0, child.x + child.wid, curses.ACS_VLINE, self.hei)
+                except curses.error:
+                    pass
+
+    def draw(self):
+        if self.need_clear:
+            self.win.erase()
+            self.need_redraw = True
+            self.need_clear = False
+        for tab in self.fm.tabs.values():
+            directory = tab.thisdir
+            if directory:
+                directory.load_content_if_outdated()
+                directory.use()
+        DisplayableContainer.draw(self)
+        if self.settings.draw_borders:
+            draw_borders = self.settings.draw_borders.lower()
+            if draw_borders in ['both', 'true']:
+                border_types = ['separators', 'outline']
+            else:
+                border_types = [draw_borders]
+            self._draw_borders(border_types)
+        if self.draw_bookmarks:
+            self._draw_bookmarks()
+        elif self.draw_hints:
+            self._draw_hints()
+        elif self.draw_info:
+            self._draw_info(self.draw_info)
+
+    def poke(self):
+        ViewBase.poke(self)
+
+        if self.old_draw_borders != self.settings.draw_borders:
+            self.resize(self.y, self.x, self.hei, self.wid)
+            self.old_draw_borders = self.settings.draw_borders
