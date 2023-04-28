6716c82ac4d68368f0a3fd29a8237e3709218e3e
phantasea <phantasea@gmail.com>
2023-04-28 19:57:31 +0300

    DIY: Display count of files to be deleted

--- ranger/config/commands.py
+++ ranger/config/commands.py
@@ -796,7 +796,7 @@ class delete(Command):
         confirm = self.fm.settings.confirm_on_delete
         if confirm != 'never' and (confirm != 'multiple' or many_files):
             self.fm.ui.console.ask(
-                "Confirm deletion of: %s (y/N)" % ', '.join(files),
+                "Confirm deletion of {0} files: {1} (y/N)".format(len(files), ', '.join(files)),
                 partial(self._question_callback, files),
                 ('n', 'N', 'y', 'Y'),
             )
@@ -858,7 +858,7 @@ class trash(Command):
         confirm = self.fm.settings.confirm_on_delete
         if confirm != 'never' and (confirm != 'multiple' or many_files):
             self.fm.ui.console.ask(
-                "Confirm deletion of: %s (y/N)" % ', '.join(file_names),
+                "Confirm deletion of {0} files: {1} (y/N)".format(len(files), ', '.join(file_names)),
                 partial(self._question_callback, files),
                 ('n', 'N', 'y', 'Y'),
             )
