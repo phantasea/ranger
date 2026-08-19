e9e84830ae1f67b431ffd79e53d14c151dce0868
Yuni <hut@hut.pm>
2026-08-20 01:28:15 +0200

    fix py2 CI related to `open(..., encoding="utf-8")`

--- tests/ranger/container/test_tags.py
+++ tests/ranger/container/test_tags.py
@@ -1,5 +1,6 @@
-from __future__ import (absolute_import, division, print_function)
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 
+from io import open
 import os
 import pytest
 
